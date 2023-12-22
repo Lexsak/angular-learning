011d1a7961df1329f0e3a4e9fa829a1fffafe8e6
Lexsak <bartoszpiprek123@gmail.com>
2023-12-22 18:48:36 +0100

    changing colors works

--- src/app/layouts/change-color/change-color.component.ts
+++ src/app/layouts/change-color/change-color.component.ts
@@ -1,10 +1,20 @@
+// change-color.component.ts
+
 import { Component } from '@angular/core';
+import { ColorService } from 'src/app/color.service';
 
 @Component({
   selector: 'app-change-color',
   templateUrl: './change-color.component.html',
-  styleUrls: ['./change-color.component.css']
+  styleUrls: ['./change-color.component.css'],
 })
 export class ChangeColorComponent {
+  color: string = '#808080';
 
+  constructor(private colorService: ColorService) {}
+
+  changeColor(event: any) {
+    const color = event?.target?.value 
+    this.colorService.setMainColor(color);
+  }
 }
