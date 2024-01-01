791ecbc8c6808edefd1367d6454c0efbe258da66
Lexsak <bartoszpiprek123@gmail.com>
2024-01-01 21:09:58 +0100

    copyColor logic work

--- src/app/layouts/copy-color/copy-color.component.ts
+++ src/app/layouts/copy-color/copy-color.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { ColorService } from 'src/app/color.service';
 
 @Component({
   selector: 'app-copy-color',
   templateUrl: './copy-color.component.html',
-  styleUrls: ['./copy-color.component.css']
+  styleUrls: ['./copy-color.component.css'],
 })
 export class CopyColorComponent {
+  constructor(private colorService: ColorService) {}
 
+  getBackgroundColor() {
+    return this.colorService.textColor;
+  }
+
+  getSecondColor() {
+    return this.colorService.backgroundColor;
+  }
+
+  getAccentColor() {
+    return this.colorService.accentColor;
+  }
 }
